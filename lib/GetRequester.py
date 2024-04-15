8fb5df5e6cebb1efeda7f684d973b26354de8d47
GlenWamaru <garpirungu@gmail.com>
2024-04-15 13:17:18 +0300

    Initial commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,13 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        response.raise_for_status()
+        return response.content.decode('utf-8')
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
